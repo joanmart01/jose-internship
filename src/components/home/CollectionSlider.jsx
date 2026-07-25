c50edf5d65de7fade0a5b139ca88ef34109cf01f
joanmart01 <joanmart_castillo@hotmail.com>
2026-07-25 16:11:21 -0400

    new items now dyanimic and in carousel

--- src/components/home/CollectionSlider.jsx
+++ src/components/home/CollectionSlider.jsx
@@ -1,6 +1,5 @@
 import React, { useRef } from 'react';
 import Slider from "react-slick";
-import { Link } from 'react-router-dom';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "../../css/carousel-arrows.css";
@@ -69,48 +68,7 @@ const CollectionSlider = ({array}) => {
 
   return (
     <Slider ref={slider => sliderRef=slider} {...settings}>
-        {array?
-            array.map((collection) => (
-                <div className='slide__wrap' key={collection.id}>
-                    <div className="nft_coll">
-                        <div className="nft_wrap">
-                            <Link to="/item-details">
-                            <img src={collection.nftImage} className="lazy img-fluid" alt="" />
-                            </Link>
-                        </div>
-                        <div className="nft_coll_pp">
-                            <Link to="/author">
-                            <img className="lazy pp-coll" src={collection.authorImage} alt="" />
-                            </Link>
-                            <i className="fa fa-check"></i>
-                        </div>
-                        <div className="nft_coll_info">
-                            <Link to="/explore">
-                            <h4>{collection.title}</h4>
-                            </Link>
-                            <span>`ERC-{collection.code}`</span>
-                        </div>
-                    </div>
-                </div>
-            ))
-            :
-            new Array(4).fill().map((elem, index)=> (
-                <div className='slide__wrap' key={index}>
-                    <div className="nft_coll skeleton__slide">
-                        <div className="nft_wrap skeleton nft__skeleton"></div>
-                        <div className="nft_coll_pp img__skeleton--wrapper skeleton">
-                            <div className='check__skeleton--wrap'><i className="fa fa-check check__skeleton"></i></div>
-                            
-                        </div>
-                        <div className="skeleton__text--wrap">
-                            <div className='skeleton skeleton__text longer-text__skeleton'></div>
-                            <div className='skeleton skeleton__text'></div>
-                        </div>
-                    </div>
-                </div>
-            ))
-        }
-      
+        {array}
     </Slider>
   )
 }
--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { Link } from 'react-router-dom';
 import axios from "axios";
 import CollectionSlider from "./CollectionSlider";
 
@@ -26,7 +27,48 @@ const HotCollections = () => {
               <div className="small-border bg-color-2"></div>
             </div>
           </div>
-          {<CollectionSlider array={!loading && collections}/> }
+          {<CollectionSlider 
+            array={loading?
+              new Array(4).fill().map((elem, index)=> (
+                <div className='slide__wrap' key={index}>
+                    <div className="nft_coll skeleton__slide">
+                        <div className="nft_wrap skeleton nft__skeleton"></div>
+                        <div className="nft_coll_pp img__skeleton--wrapper skeleton">
+                            <div className='check__skeleton--wrap'><i className="fa fa-check check__skeleton"></i></div>
+                            
+                        </div>
+                        <div className="skeleton__text--wrap">
+                            <div className='skeleton skeleton__text longer-text__skeleton'></div>
+                            <div className='skeleton skeleton__text'></div>
+                        </div>
+                    </div>
+                </div>
+            ))
+            :
+              collections.map((collection) => (
+                  <div className='slide__wrap' key={collection.id}>
+                      <div className="nft_coll">
+                          <div className="nft_wrap">
+                              <Link to="/item-details">
+                              <img src={collection.nftImage} className="lazy img-fluid" alt="" />
+                              </Link>
+                          </div>
+                          <div className="nft_coll_pp">
+                              <Link to="/author">
+                              <img className="lazy pp-coll" src={collection.authorImage} alt="" />
+                              </Link>
+                              <i className="fa fa-check"></i>
+                          </div>
+                          <div className="nft_coll_info">
+                              <Link to="/explore">
+                              <h4>{collection.title}</h4>
+                              </Link>
+                              <span>`ERC-{collection.code}`</span>
+                          </div>
+                      </div>
+                  </div>
+              ))
+             }/> }
         </div>
       </div>
     </section>
