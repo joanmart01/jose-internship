8af4bda05b5dca056ecef12496ac30d5179ab9db
joanmart01 <joanmart_castillo@hotmail.com>
2026-08-03 11:16:15 -0400

    load-more function fixed

--- src/components/explore/ExploreItems.jsx
+++ src/components/explore/ExploreItems.jsx
@@ -5,67 +5,32 @@ import ItemCardSkeleton from "../UI/ItemCardSkeleton";
 import "../../css/explore-items.css";
 
 const ExploreItems = () => {
-  
-  const [rows, setRows] = useState(2);
-  const rowHeight = 477.33;
-  const initHeight = rowHeight*rows;
-  const [areaHeight, setHeight] = useState(initHeight);
-  const [cols, setCols] = useState(0);
 
-  const [items, setItems] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [allItems, setAllItems] = useState([]);
+  const [currItems, setCurrentItems] = useState([]);
+  const [currLimit, setCurrentLimit] = useState(0);
+  const rowSize = 4;
+  const initialNum = 8;
   
-  const areaRef = useRef(null);
   const buttRef = useRef(null);
   
   useEffect(()=>{
-    fetchItems();
+    fetchItems();    
     setLoading(false);
-  }, [])
-
-  useEffect(()=> {
-    if(areaRef.current) {
-      const observer = new ResizeObserver((entries)=>{
-        for(let entry of entries)
-          calcColumns(entry.contentRect.width);
-      })
-      observer.observe(areaRef.current);
-    }
   }, [])
 
   async function fetchItems() {
     const promise = await axios.get("https://us-central1-nft-cloud-functions.cloudfunctions.net/explore");
-    setItems(promise.data);
+    setAllItems(promise.data);
+    setCurrentItems(promise.data.slice(0, initialNum));
+    setCurrentLimit(initialNum);
   }
 
-  /* When pg width is 991.33 -> box width is 903.1875 -> cols change from 4 to 2
-    When pg width is 575.33 -> box width is 500 -> cols change from 2 to 1 */
-  function calcColumns(width) {
-    let currCols = width>903.18 ? 4
-                  : width>500 ? 2
-                  : 1;
-    let diff = currCols - cols;
-    setCols(currCols);
-    if(diff>0 && areaHeight > initHeight) reduceAreaHeight(diff===1? 1 : 2);
-  }
-
-  function reduceAreaHeight(num){
-    setHeight(areaHeight - num*rowHeight);
-    let currRows = rows - num;
-    setRows(currRows);
-    detLoadButtonDisabled(currRows);
-  }
-
-  function extendAreaHeight(){
-    setHeight(areaHeight + rowHeight);
-    let currRows = rows + 1;
-    setRows(currRows);
-    detLoadButtonDisabled(currRows);
-  }
-
-  function detLoadButtonDisabled(currRows){
-    if(buttRef.current)
-      buttRef.current.disabled = currRows*cols >= 16;
+  function loadMoreItems() {
+    let nextLimit = currLimit + rowSize;
+    setCurrentItems([...currItems, ...allItems.slice(currLimit, nextLimit)]);
+    setCurrentLimit(nextLimit);
   }
 
   return (
@@ -79,9 +44,8 @@ const ExploreItems = () => {
         </select>
       </div>
 
-      <div ref={areaRef} 
-           style={{display: "flex", flexWrap: "wrap", overflow:"hidden", 
-                   width: "100%", height: areaHeight + "px"}}>
+      <div style={{display: "flex", flexWrap: "wrap", overflow:"hidden", 
+                   width: "100%"}}>
         {loading?
         new Array(8).fill(0).map((_, index) => (
           <div
@@ -93,11 +57,11 @@ const ExploreItems = () => {
           </div>
         ))
         :
-        items.map((item) => (
-          <div key={item.id}
+        currItems.map((item, index) => (
+          <div key={index}
               className="d-item col-lg-3 col-md-6 col-sm-6 col-xs-12"
               style={{ display: "block", backgroundSize: "cover" }} >
-            <ItemCard item={item} key={item.id} />
+            <ItemCard item={item}/>
           </div>
         ))
         }
@@ -107,7 +71,7 @@ const ExploreItems = () => {
         <button ref={buttRef}
                 id="loadmore" 
                 className="btn-main lead"
-                onClick={extendAreaHeight}>
+                onClick={loadMoreItems}>
           Load more
         </button>
       </div>
