86af25457815103910da2812d6756ec3d21d8f10
joanmart01 <joanmart_castillo@hotmail.com>
2026-08-06 11:08:04 -0400

    finished fixing warnings

--- src/components/UI/Timer.jsx
+++ src/components/UI/Timer.jsx
@@ -7,33 +7,31 @@ const Timer = ({expTime}) => {
     const [minText, setMinText] = useState("0");
     const timerRef = useRef(null);
 
-    let milliseconds = 10;
-    let seconds;
-    let minutes;
-    let hours;
-
     useEffect(()=> {
         if(expTime > Date.now()) {
+            let milliseconds = 10,
+                seconds, 
+                minutes, 
+                hours;
+
             const timerID = setInterval(() => {
-                if(milliseconds > 0) updateTimer();
+                if(milliseconds > 0) {
+                    milliseconds = expTime - Date.now();
+                    if(milliseconds<0) return;
+                    seconds = milliseconds/1000;
+                    minutes = seconds/60;
+                    hours = minutes/60;
+
+                    if(timerRef.current) {
+                        setSecText((Math.floor(seconds % 60)).toString().padStart(2, "0"));
+                        setMinText((Math.floor(minutes % 60)).toString().padStart(2, "0"));
+                        setHourText((Math.floor(hours)).toString());
+                    }
+                }
             }, 1000)
             return ()=> clearInterval(timerID);
         }
-    }, [])
-
-    function updateTimer() {
-        milliseconds = expTime - Date.now();
-        if(milliseconds<0) return;
-        seconds = milliseconds/1000;
-        minutes = seconds/60;
-        hours = minutes/60;
-        
-        if(timerRef.current) {
-            setSecText((Math.floor(seconds % 60)).toString().padStart(2, "0"));
-            setMinText((Math.floor(minutes % 60)).toString().padStart(2, "0"));
-            setHourText((Math.floor(hours)).toString());
-        }
-    }
+    }, [expTime])
 
     return (
         <div ref={timerRef} className="de_countdown">{`${hourText}h ${minText}m ${secText}s`}</div>
